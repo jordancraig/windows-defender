c1422f769478558accf70ab561e3973baa8f1e06
Jordan Craig <jordan.craig@sqa.org.uk>
2017-12-29 15:28:23 +0000

    Small changes to Berksfile and README.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Jordan Craig'
 maintainer_email 'jordan@jwac.me'
 license 'Apache-2.0'
 description 'Installs/Configures Windows Defender'
-long_description 'Manages Windows Defender on modern Windows Operating Systems.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports 'windows'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
