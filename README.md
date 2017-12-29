2b3a450bdb057dae4714e6656f907a42c103f3ee
Jordan Craig <jordan.craig@sqa.org.uk>
2017-12-29 14:26:45 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# windows-defender
+# Windows Defender
 
-TODO: Enter the cookbook description here.
+Provides a set of specific resources that can be used to manage Windows Defender on supported systems.
 
+## Requirements
+
+### Platforms
+- Windows 2016
+- Windows 2012R2
+- Windows 2012
+- Windows 10
+- Windows 8.1
+- Windows 8
+
+### Chef
+- Chef 12.7+
--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,10 @@
 name 'windows-defender'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures windows-defender'
-long_description 'Installs/Configures windows-defender'
+maintainer 'Jordan Craig'
+maintainer_email 'jordan@jwac.me'
+license 'Apache 2.0'
+description 'Installs/Configures Windows Defender'
+long_description 'Manages Windows Defender on modern Windows Operating Systems.'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/windows-defender/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/windows-defender'
+issues_url 'https://github.com/jordancraig/windows-defender/issues'
+source_url 'https://github.com/jordancraig/windows-defender'
