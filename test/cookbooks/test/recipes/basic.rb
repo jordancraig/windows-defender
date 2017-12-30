41cd41c5f75f7489e53904552cfdd02faa834eb6
Jordan Craig <jordan.craig94@gmail.com>
2017-12-30 11:32:35 +0000

    Oops.

--- test/cookbooks/test/recipes/basic.rb
+++ test/cookbooks/test/recipes/basic.rb
@@ -1,6 +1,6 @@
 # Enable Defender feature
 windows_defender_config 'enable' do
-  action :enable
+  action :enable_feature
 end
 # Check that status returns a value.
 windows_defender_status 'status' do
